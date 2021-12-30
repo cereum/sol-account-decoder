3e53c5deae9b64d055f5e8c88b0ad7e1c9631a08
Gabriel Bryk <gbryk@hawk.iit.edu>
2021-12-30 17:19:17 -0600

    hmmmm

--- src/pages/Decoder.tsx
+++ src/pages/Decoder.tsx
@@ -1,84 +1,38 @@
 import { useSolana } from "@saberhq/use-solana";
 import { AccountInfo, PublicKey } from "@solana/web3.js";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { AccountViewer } from "../components/AccountViewer";
 
-import {HexViewer} from 'react-hexviewer-ts';
+import { PublicKeyInput } from "../components/PublicKeyInput";
 
-type DecoderState = "input" | "parsed" | "unparsed";
 
 export const Decoder = () => {
   const { connection } = useSolana();
-  const [decoderState, setDecoderState] = useState<DecoderState>("input");
   const [accountInfo, setAccountInfo] = useState<AccountInfo<Buffer> | null>();
 
-  const handleInput = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
+  const getAccountInfo = async () => {
     try {
-      const publicKey = new PublicKey(event.target.value);
-      console.log(publicKey);
-      const accountInfo = await connection.getAccountInfo(publicKey);
+      const accountInfo = await connection.getAccountInfo(publicKey!);
       setAccountInfo(accountInfo);
-      setDecoderState("parsed");
     } catch (error) {
       console.log(error);
     }
   };
-  const switchState = (state: DecoderState) => {
-    switch (state) {
-      case "input": {
-        return (
-          <div>
-            <h2
-              className="text-3xl font-extrabold text-gray-600"
-              style={{ textAlign: "center" }}
-            >
-              Enter Account Address
-            </h2>
-            <input
-              type="text"
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                handleInput(event);
-              }}
-              placeholder="Address"
-              className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-            />
-          </div>
-        );
-      }
-      case "parsed": {
-        console.log(accountInfo);
-        return accountInfo ? (
-          <div className="min-h-screen bg-yellow-50 py-8 px-4">
-            <div className="flex flex-col items-center max-w-7xl mx-auto">
-              <HexViewer
-                hex
-                rowLength={64}
-                setLength={4}
-              >
-                {accountInfo.data.toString("hex")}
-              </HexViewer>
-            </div>
-          </div>
-        ) : null;
-      }
-      case "unparsed": {
-        return (
-          <input
-            type="text"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              handleInput(event);
-            }}
-            placeholder="Address"
-            className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-          />
-        );
-      }
+  useEffect(() => {
+    if(publicKey){
+      getAccountInfo()
     }
-  };
+  } ,[publicKey])
+
   return (
     <div className="min-h-screen bg-yellow-50 py-8 px-4">
       <div className="flex flex-col items-center max-w-7xl mx-auto">
-        {switchState(decoderState)}
+        {!publicKey?
+          <PublicKeyInput setPublicKey={setPublicKey}></PublicKeyInput>:null
+        }
+        {accountInfo?
+          <AccountViewer accountInfo={accountInfo!}></AccountViewer>:null
+        }
       </div>
     </div>
   );
