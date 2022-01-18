fcb5960e319096097acab3fa7381a63fb998c9f9
cereum <49958069+cereum@users.noreply.github.com>
2022-01-18 13:33:31 -0600

    Added program IDL parsing

--- src/pages/DecodeAnchor.tsx
+++ src/pages/DecodeAnchor.tsx
@@ -1,15 +1,50 @@
 import { Idl } from "@project-serum/anchor";
-import { Address, Program } from "@project-serum/anchor/dist/cjs/program";
+import { Program } from "@project-serum/anchor/dist/cjs/program";
 import { useSolana } from "@saberhq/use-solana";
 import { PublicKey } from "@solana/web3.js";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { SchemaInput } from "../components/SchemaInput";
+import { Button } from "../elements";
 
 export const DecodeAnchor = () => {
-  const { accountPubkey } = useParams<{ accountPubkey: string }>();
+  const [program, setProgram] = useState<Program>();
+  const { provider } = useSolana();
+  const [idl, setIDL] = useState<any>();
 
-  return (
-    <div>
-      <input></input>
+  const handleSubmit = () => {
+    try {
+      setProgram(
+        new Program(
+          idl! as Idl,
+          new PublicKey(idl!.metadata.address ? idl!.metadata.address : 0),
+          provider
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const validateIDL = (event: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const idlJSON = JSON.parse(event.target.value);
+      setIDL(idlJSON);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return !program ? (
+    <div className="min-h-screen bg-yellow-50 py-8 px-4">
+      <div className="flex flex-col items-center max-w-7xl mx-auto">
+        <SchemaInput onChange={validateIDL} />
+        <Button className="px-3 py-3 my-4 w-full" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </div>
     </div>
+  ) : (
+    <>
+     
+    </>
   );
 };
