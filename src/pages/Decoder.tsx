1f9d2d7060eb82421944c2f5b53ab4735b0994a3
cereum <49958069+cereum@users.noreply.github.com>
2022-01-17 19:16:39 -0600

    Added error message for valid, yet does not have AccountInfo

--- src/pages/Decoder.tsx
+++ src/pages/Decoder.tsx
@@ -4,20 +4,21 @@ import { useState, useEffect } from "react";
 import { AccountViewer } from "../components/AccountViewer";
 
 import { PublicKeyInput } from "../components/PublicKeyInput";
+import { Box } from "../elements";
 
 type DecoderState = "input" | "parsed" | "unparsed";
 
 export const Decoder = () => {
   const { connection } = useSolana();
   const [decoderState, setDecoderState] = useState<DecoderState>("input");
-  const [accountInfo, setAccountInfo] = useState<AccountInfo<Buffer>>();
+  const [accountInfo, setAccountInfo] = useState<AccountInfo<Buffer> | null>();
   const [accountKey, setAccountKey] = useState<PublicKey>();
   const [, setSchema] = useState<{}>();
 
   const getAccountInfo = async () => {
     try {
       const accountInfo = await connection.getAccountInfo(accountKey!);
-      setAccountInfo(accountInfo!);
+      setAccountInfo(accountInfo);
     } catch (error) {
       console.log(error);
     }
@@ -42,7 +43,16 @@ export const Decoder = () => {
       case "unparsed": {
         return accountInfo ? (
           <AccountViewer accountInfo={accountInfo}></AccountViewer>
-        ) : null;
+        ) : (
+          <Box.Center>
+            <p
+              className="text-2xl font-extrabold text-gray-600"
+              style={{ textAlign: "center" }}
+            >
+              Whoops! We can't seem to find any information about this account.
+            </p>
+          </Box.Center>
+        );
       }
       case "parsed": {
         return <p>Schema Parser</p>;
