ec5a001377bdba58d83bc69510c20de32d3b0eeb
cereum <49958069+cereum@users.noreply.github.com>
2022-01-17 18:50:45 -0600

    fixed typo

--- src/components/PublicKeyInput.tsx
+++ src/components/PublicKeyInput.tsx
@@ -61,7 +61,7 @@ export const PublicKeyInput = ({
         placeholder="Address"
         className="px-3 py-3 my-4 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
       />
-      <div className="flex flex-row items-centerc">
+      <div className="flex flex-row items-center">
         <input
           type="checkbox"
           checked={checked}
